2442ea6c78437a469b8051c0ce65d80e20b228c6
RgDivine <boadidivine111@gmail.com>
2024-05-19 17:05:39 +0000

    Second commit: Formatted array string

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,3 +10,23 @@ const processArray = (arr) => {
 }
 num = [2,5,6,9,12,15]
 console.log(processArray(num));
+
+
+const formatArrayStrings = (strings, nums) => {
+    const processedNums = processArray(nums);
+
+    return strings.map((str, i) => {
+    if (processedNums[i] % 2 == 0) {
+        return str.toUpperCase();
+    } else {
+        return str.toLowerCase();
+    }
+    });
+}
+
+const strings = ["hello", "world", "Student"]
+const nums = [1,2,3,4,5,6]
+
+console.log(formatArrayStrings(strings, nums))
+
+module.exports = formatArrayStrings;
